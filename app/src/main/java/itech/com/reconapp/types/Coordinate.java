fa566863f0d300f5fa7bb26469a8b2f02a76fa1d
tugcaeker <tugcaeker@gmail.com>
2017-03-15 02:23:32 +0300

    Haversine Distance

--- app/src/main/java/itech/com/reconapp/types/Coordinate.java
+++ app/src/main/java/itech/com/reconapp/types/Coordinate.java
@@ -1,11 +1,31 @@
 package itech.com.reconapp.types;
 
 public class Coordinate {
+    public int EarthRadius = 6371;
     public double latitude;
     public double longitude;
 
-    Coordinate(double lat, double lon){
+    Coordinate(double lat, double lon) {
         this.latitude = lat;
         this.longitude = lon;
+    }
+
+
+    public double distance(Coordinate point) {
+        double dLat = Math.toRadians((point.latitude - latitude));
+        double dLong = Math.toRadians((point.longitude - longitude));
+
+        double startLatInRadians = Math.toRadians(latitude);
+        double endLatInRadians = Math.toRadians(point.latitude);
+
+        double a = haversine(dLat) + Math.cos(latitude) * Math.cos(point.latitude) * haversine(dLong);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return EarthRadius * c;
+
+    }
+
+    private double haversine(double val) {
+        return Math.pow(Math.sin(val / 2), 2);
     }
 }
