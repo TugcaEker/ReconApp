913a3d1b4b6f9f238ef37e9aac5e4a127c1fdbff
tugcaeker <tugcaeker@gmail.com>
2017-03-15 03:58:30 +0300

    Visual Center of Polygon

--- app/src/main/java/itech/com/reconapp/types/Polygon.java
+++ app/src/main/java/itech/com/reconapp/types/Polygon.java
@@ -6,6 +6,12 @@ import java.util.List;
 
 public class Polygon {
     public Coordinate corners[];
+    public int count;
+    public Coordinate center;
+
+    private void update() {
+        this.count = corners.length;
+    }
 
     public boolean isInside(Coordinate point) {
         int sides = corners.length - 1;
@@ -22,5 +28,43 @@ public class Polygon {
             j = i;
         }
         return pointStatus;
+    }
+
+    public Coordinate center() {
+        this.update();
+        if (corners.length == 1) {
+            return corners[0];
+        }
+
+        double x = 0;
+        double y = 0;
+        double z = 0;
+
+        for (Coordinate c : corners) {
+            double latitude = c.latitude * Math.PI / 180;
+            double longitude = c.longitude * Math.PI / 180;
+
+            x += Math.cos(latitude) * Math.cos(longitude);
+            y += Math.cos(latitude) * Math.sin(longitude);
+            z += Math.sin(latitude);
+        }
+
+        int total = this.count;
+
+        x = x / total;
+        y = y / total;
+        z = z / total;
+
+        double center_lon = Math.atan2(y, x);
+        double center_lat = Math.atan2(z, Math.sqrt(x * x + y * y));
+        return new Coordinate(
+                center_lat * 180 / Math.PI,
+                center_lon * 180 / Math.PI
+        );
+    }
+
+    public void setCenter(){
+        Coordinate calculated = center();
+        this.center = calculated;
     }
 }
