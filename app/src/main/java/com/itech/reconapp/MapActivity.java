afc9dbfa4e01fff1e2d2647949abfe0e6f469e0d
tugcaeker <tugcaeker@gmail.com>
2017-04-05 03:30:46 +0300

    MapActivity methods overloaded.

--- app/src/main/java/com/itech/reconapp/MapActivity.java
+++ app/src/main/java/com/itech/reconapp/MapActivity.java
@@ -13,6 +13,8 @@ import com.reconinstruments.maplibrary.mapview.MapView;
 
 public class MapActivity extends FragmentActivity {
 
+    private static final String TAG = "MapActivity";
+
     MapFragment mMapFragment = null;
     MapView mMap = null;
 
@@ -35,6 +37,44 @@ public class MapActivity extends FragmentActivity {
             mMap = null;
         }
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Intent intent = new Intent("RECON_ACTIVATE_GPS");
+        intent.putExtra("RECON_GPS_CLIENT", TAG);
+        sendBroadcast(intent);
+    }
+
+    @Override
+    public void onPause()   {
+        super.onPause();
+        Intent intent = new Intent("RECON_DEACTIVATE_GPS");
+        intent.putExtra("RECON_GPS_CLIENT", TAG);
+        sendBroadcast(intent);
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (mMapFragment != null && mMapFragment.onKeyDown(keyCode, event)) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (mMapFragment != null && mMapFragment.onKeyUp(keyCode, event)) {
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
     }
 
 }
