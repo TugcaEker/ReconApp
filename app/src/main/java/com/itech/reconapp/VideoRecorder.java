cb9d73640f0e4b32abe2c4ec7b6ce76bb5a64ca4
tugcaeker <tugcaeker@gmail.com>
2017-04-19 03:56:59 +0300

    New class VideoRecorder added.

--- app/src/main/java/com/itech/reconapp/VideoRecorder.java
+++ app/src/main/java/com/itech/reconapp/VideoRecorder.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import java.io.File;
 import java.io.IOException;
 
+
 public class VideoRecorder {
 
     private static final String TAG = "VideoRecorder";
@@ -30,6 +31,7 @@ public class VideoRecorder {
         this.camera = camera;
         videoValues = StorageUtils.getVideoData(camProfile,recordingStartTime);
         tmpVideoFile = StorageUtils.getVideoPath(videoValues)+".tmp";
+        prepareMediaRecorder(tmpVideoFile);
     }
 
     public void startRecording() {
@@ -40,18 +42,59 @@ public class VideoRecorder {
     public void stopRecording() {
         boolean saveVideo = false;
         try {
-            mediaRecorder.stop();
+            mediaRecorder.stop(); // stop the recording
             saveVideo = true;
         } catch(RuntimeException e) {
             boolean deleted = new File(tmpVideoFile).delete();
             Log.e(TAG,"Error stopping media recorder "+(deleted?"deleted tmp fail":"failed to delete tmp file"),e);
         } finally {
-            /* release olacak */
+            releaseMediaRecorder();
         }
-        if(saveVideo){
-            /* kayit yapacagiz */
-        }
+        if(saveVideo)
+            storeVideo();
     }
 
+    private void storeVideo() {
+        long duration = System.currentTimeMillis() - recordingStartTime;
+        if (duration <= 0) {
+            Log.w(TAG, "Video duration <= 0 : " + duration);
+        }
+        StorageUtils.insertVideo(activity, tmpVideoFile, videoValues, duration);
+    }
 
+    private boolean prepareMediaRecorder(String fileName) {
+        mediaRecorder = new MediaRecorder();
+
+        camera.unlock();
+        mediaRecorder.setCamera(camera);
+
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+        mediaRecorder.setProfile(camProfile);
+
+        mediaRecorder.setOutputFile(fileName);
+        mediaRecorder.setMaxDuration(MAX_DURATION * 1000);
+        mediaRecorder.setMaxFileSize(50000000);
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            releaseMediaRecorder();
+            return false;
+        } catch (IOException e) {
+            releaseMediaRecorder();
+            return false;
+        }
+        return true;
+    }
+
+    public void releaseMediaRecorder() {
+        if (mediaRecorder != null) {
+            mediaRecorder.reset();
+            mediaRecorder.release();
+            mediaRecorder = null;
+            camera.lock();
+        }
+    }
 }
